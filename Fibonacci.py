8d98fa38b16cc55dcc7157a36ce13e888091cd0d
Andre Galioni <agalioni@recovery.local>
2020-03-26 21:54:44 +0000

    Fibonacci sequence working

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,2 +1,12 @@
 def fib(n):
-    return 1;
+    if (n == 0) :
+        return 0
+    elif (n == 1) :
+         return 1
+    else :
+         return (fib(n-2) + fib(n-1))
+
+    return sum
+
+
+print(fib(9))
